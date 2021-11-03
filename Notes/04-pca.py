f5e06f40a1525e6a3d803861024edc84eadee828
AaronWenbu <aaronwen@bu.edu>
2021-11-03 13:19:37 -0400

    Create 04-pca.py

---
+++ Notes/04-pca.py
@@ -0,0 +1,15 @@
+# warmup exercise
+# questrom.datasets.diamonds
+# 1. write SQL to get the diamonds table from Big Query
+# 2. keep only numeric columns (pandas can be your friend here!)
+# 3. use kmeans to fit a 5 cluster solution
+# 4. create a boxplot of the column carat by cluster label (one boxplot for each cluster)
+# OPTIONAL:  silhouette plot for the records   <------this will take a few minutes to run, we will discuss
+
+
+
+
+## your exercise
+## fit a pca model for the diamonds
+## make sure you think about data pre-processing
+## plot out the explained variance of the new PCAs based on your dataset
